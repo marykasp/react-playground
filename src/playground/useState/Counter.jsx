cce9b8966907fbdb0e7af59313122d71eea0a36a
marykasp <kasparian.mary@gmail.com>
2026-02-21 14:42:40 -0600

    add styling to simple counter app

--- src/playground/useState/Counter.jsx
+++ src/playground/useState/Counter.jsx
@@ -2,12 +2,35 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h2>useState Counter</h2>
-      <p>{count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <h2>Counter</h2>
+      <p className="font-bold text-2xl max-w-fit my-3 py-2 px-4 border-2 border-slate-500 rounded-sm">
+        {count}
+      </p>
+      <div class="row flex">
+        <button
+          class="rounded-md rounded-r-none bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+          onClick={() => setCount((c) => c + 1)}
+        >
+          +
+        </button>
+        <button
+          class="rounded-none bg-slate-800 py-2 px-4 border-l border-r border-slate-700 text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+          onClick={() => setCount((c) => c - 1)}
+        >
+          -
+        </button>
+        <button
+          class="rounded-md rounded-l-none bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          type="button"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
