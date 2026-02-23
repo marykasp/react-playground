c193cac9c7ce07aaa349a4d9e9988bb06ea90f06
marykasp <kasparian.mary@gmail.com>
2026-02-23 12:18:35 -0600

    create task lists that display each tasks in the state tasks array, the task item will handle the update and delete functionality through context and reducer

--- src/playground/context/task-manager/components/TaskItem.jsx
+++ src/playground/context/task-manager/components/TaskItem.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { TaskContext } from "../context/TaskContext";
 
-const TaskItem = () => {
-  return <div>TaskItem</div>;
+const TaskItem = ({ task }) => {
+  const { dispatch } = React.useContext(TaskContext);
+
+  const handleToggleTask = (id) => {
+    dispatch({ type: "toggle", id });
+  };
+
+  const handleDeleteTask = (id) => {
+    dispatch({ type: "delete", id });
+  };
+
+  return (
+    <div className="flex items-center justify-between bg-gray-50 px-4 py-2 rounded-lg shadow-sm">
+      <span
+        onClick={() => handleToggleTask(task.id)}
+        className={`cursor-pointer ${task.completed ? "line-through text-gray-400" : "text-gray-500"}`}
+      >
+        {task.name}
+      </span>
+
+      <button
+        onClick={() => handleDeleteTask(task.id)}
+        className="text-red-500 hover:text-red-700 text-sm cursor-pointer"
+      >
+        Delete
+      </button>
+    </div>
+  );
 };
 
 export default TaskItem;
--- src/playground/context/task-manager/components/TaskList.jsx
+++ src/playground/context/task-manager/components/TaskList.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { TaskContext } from "../context/TaskContext";
+import TaskItem from "./TaskItem";
 
 export const TaskList = () => {
-  const { state, dispatch } = React.useContext(TaskContext);
+  const { state } = React.useContext(TaskContext);
+  const { tasks, filter } = state;
 
-  return <div>TaskList</div>;
+  // filter tasks based on the filter property of state
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "ACTIVE") return !task.completed;
+    if (filter === "COMPLETED") return task.completed;
+    // for all other cases the condition will always be true since want to show every task item
+    return true;
+  });
+
+  // if no filtered tasks display a paragraph insted of list
+  if (!filteredTasks.length) {
+    return <p className="text-center text-gray-500">No tasks yet.</p>;
+  }
+
+  return (
+    <div className="space-y-2">
+      {filteredTasks.map((task) => (
+        <TaskItem key={task.id} task={task} />
+      ))}
+    </div>
+  );
 };
 
 export default TaskList;
